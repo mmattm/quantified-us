3859516a7aa1e707afc0e4cd73998d01d748f78a
Matthieu Minguet <minguet.mat@gmail.com>
2015-07-22 19:45:28 +0200

    fix external

--- app/assets/javascripts/external.js
+++ app/assets/javascripts/external.js
@@ -1,30 +1,39 @@
+
 if (("standalone" in window.navigator) && window.navigator.standalone) {
 
     var noddy, remotes = false;
 
-    // document.addEventListener('click', function(event) {
-
-    //     noddy = event.target;
-
-    //     while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-    //         noddy = noddy.parentNode;
-    //     }
-
-    //     if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
-    //         event.preventDefault();
-    //         document.location.href = noddy.href;
-    //     }
-
-    // }, false);
-
-    var a=document.getElementsByClassName("external");
-    for(var i=0;i<a.length;i++)
-    {
-        a[i].onclick=function()
-        {
-        	console.log("external");
-            window.location=this.getAttribute("href");
-            return false
+    document.addEventListener('click', function(event) {
+        noddy = event.target;
+        
+        while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+            noddy = noddy.parentNode;
         }
-    }
+        if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
+           // event.preventDefault();
+            if ( (" " + noddy.className + " ").replace(/[\n\t]/g, " ").indexOf(" external ") > -1 ) {
+            	console.log("YES");
+            	event.preventDefault();
+            	document.location.href = noddy.href;
+            }
+           // document.location.href = noddy.href;
+        }
+    }, false);
 }
+
+// document.addEventListener('click', function(event) {
+//     noddy = event.target;
+    
+//     while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+//         noddy = noddy.parentNode;
+//     }
+//     if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
+//        // event.preventDefault();
+//         if ( (" " + noddy.className + " ").replace(/[\n\t]/g, " ").indexOf(" external ") > -1 ) {
+//         	console.log("YES");
+//         	event.preventDefault();
+//         	document.location.href = noddy.href;
+//         }
+//        // document.location.href = noddy.href;
+//     }
+// }, false);
