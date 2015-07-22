a28f4fe715746be50b22d0d4dd8afab278d9ccb4
Matthieu Minguet <minguet.mat@gmail.com>
2015-07-22 19:29:20 +0200

    fix external

--- app/assets/javascripts/external.js
+++ app/assets/javascripts/external.js
@@ -1,20 +1,29 @@
-if(("standalone" in window.navigator) && window.navigator.standalone){
+if (("standalone" in window.navigator) && window.navigator.standalone) {
 
-var noddy, remotes = false;
+    var noddy, remotes = false;
 
-document.addEventListener('click', function(event) {
+    // document.addEventListener('click', function(event) {
 
-noddy = event.target;
+    //     noddy = event.target;
 
-while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-noddy = noddy.parentNode;
-}
+    //     while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+    //         noddy = noddy.parentNode;
+    //     }
 
-if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
-{
-event.preventDefault();
-document.location.href = noddy.href;
-}
+    //     if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
+    //         event.preventDefault();
+    //         document.location.href = noddy.href;
+    //     }
 
-},false);
+    // }, false);
+
+    var a=document.getElementsByClassName("external");
+    for(var i=0;i<a.length;i++)
+    {
+        a[i].onclick=function()
+        {
+            window.location=this.getAttribute("href");
+            return false
+        }
+    }
 }
