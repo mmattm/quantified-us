f9cd1853f22f5b8d020a4037a721659a4a3f3813
Matthieu Minguet <minguet.mat@gmail.com>
2015-07-23 14:33:53 +0200

    workers

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(:url => uri)
+ENV["REDISTOGO_URL"] = 'redis://redistogo:033a462f5971e491b77848446d345375@carp.redistogo.com:9787/'
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:url => uri)
 
 # class DataCache
 #   def self.data
