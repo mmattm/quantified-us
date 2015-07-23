7579fb3388d8865ed9bfe796c002dc5620e9ae7e
Matthieu Minguet <minguet.mat@gmail.com>
2015-07-23 16:08:12 +0200

    workers

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,5 +3,5 @@ ENV["REDISTOGO_URL"] = 'redis://redistogo:033a462f5971e491b77848446d345375@carp.
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:url => uri)
 
-Dir["/app/app/jobs/*.rb"].each { |file| require file }
+#Dir["/app/app/jobs/*.rb"].each { |file| require file }
 
